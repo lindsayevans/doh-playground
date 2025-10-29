6c5218bf36aa16e9c443f9915387be582e807a0f
Lindsay Evans <lindsay.evans@boomworks.com.au>
2025-10-29 11:32:31 +1100

    feat: get basics working

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,142 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import { Container, createTheme, MantineProvider } from "@mantine/core";
+import {
+  Button,
+  Container,
+  createTheme,
+  Flex,
+  Loader,
+  MantineProvider,
+  MultiSelect,
+  NativeSelect,
+  TextInput,
+} from '@mantine/core';
 
-import "@mantine/core/styles.css";
+import '@mantine/core/styles.css';
+import 'mantine-datatable/styles.css';
 
 const theme = createTheme({
-  fontFamily: "Inter, sans-serif",
-  primaryColor: "cyan",
+  fontFamily: 'Inter, sans-serif',
+  primaryColor: 'cyan',
 });
 
-import { Config } from "./Config";
-import { ThemeToggle } from "./components/ThemeToggle/ThemeToggle";
+import { DnsRecord, DnsRecordTypes } from './DnsRecord';
+import { DataTable } from 'mantine-datatable';
 
-export type AppProps = { config?: Config };
+const resolvers = [
+  {
+    name: 'Cloudflare',
+    url: 'https://cloudflare-dns.com/dns-query',
+  },
+  {
+    name: 'Google',
+    url: 'https://dns.google/resolve',
+  },
+];
 
-export const App: React.FC<AppProps> = (props) => {
-  const { config } = props;
+export const App: React.FC = () => {
+  const [value, setValue] = useState('querc.net');
+  const [resolver, setResolver] = useState(resolvers[0].name);
+  const [types, setTypes] = useState<string[]>(['A', 'AAAA', 'CNAME']);
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState<DnsRecord[]>([]);
+
+  const allTypes = Object.values(DnsRecordTypes)
+    .filter((x) => isNaN(x as number))
+    .map((x) => x.toString());
+
+  const buildQuery = async (
+    name: string,
+    type: string,
+    resolverUrl?: string
+  ) => {
+    const response = await fetch(`${resolverUrl}?name=${name}&type=${type}`, {
+      headers: { accept: 'application/dns-json' },
+    });
+    const data = await response.json();
+    return data;
+  };
+
+  const getResolverUrl = (name: string) => {
+    return resolvers.find((x) => x.name === name)?.url || resolvers[0].url;
+  };
+
+  const runAllQueries = async () => {
+    setLoading(true);
+    setResults([]);
+    const queries = types.map((x) =>
+      buildQuery(value, x, getResolverUrl(resolver))
+    );
+    const queryResults = await Promise.all(queries);
+
+    const results: DnsRecord[] = [];
+    queryResults.forEach((result) => {
+      if (
+        result.Status === 0 &&
+        result.Answer !== undefined &&
+        result.Answer.map
+      ) {
+        results.push(
+          ...result.Answer.map((answer: Record<string, string>) => ({
+            id: btoa(answer.type + answer.name + answer.data),
+            type: DnsRecordTypes[answer.type as unknown as number],
+            name: answer.name,
+            data: answer.data,
+            ttl: answer.TTL,
+          }))
+        );
+      }
+    });
+    setLoading(false);
+    setResults(results);
+  };
 
   return (
     <MantineProvider theme={theme} defaultColorScheme="light">
       <Container size="lg">
-        <h2>Welcome to DoH Playground!</h2>
+        <Flex align="flex-end" gap="sm" mb="md">
+          <NativeSelect
+            label="Resolver"
+            data={resolvers.map((x) => x.name)}
+            onChange={(e) => setResolver(e.target.value)}
+          />
+          <TextInput
+            label="Domain"
+            placeholder="e.g. example.com"
+            value={value}
+            onChange={(event) => setValue(event.currentTarget.value)}
+          />
+          <MultiSelect
+            label="Types"
+            data={allTypes}
+            value={types}
+            onChange={setTypes}
+            clearable
+            searchable
+          />
+          <Button variant="filled" onClick={(e) => runAllQueries()}>
+            Query
+          </Button>
+        </Flex>
 
-        <h3>Config</h3>
-        {config && <pre role="code">{JSON.stringify(config, null, 2)}</pre>}
+        {results && results.length > 0 && (
+          <DataTable
+            withTableBorder
+            borderRadius="sm"
+            withColumnBorders
+            striped
+            highlightOnHover
+            records={results}
+            columns={[
+              { accessor: 'type' },
+              { accessor: 'name' },
+              { accessor: 'data' },
+              { accessor: 'ttl', title: 'TTL' },
+            ]}
+          ></DataTable>
+        )}
 
-        <h3>Toggle theme</h3>
-        <ThemeToggle />
+        {loading && <Loader color="teal" />}
       </Container>
     </MantineProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode } from 'react';
 
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
-const $apps = document.querySelectorAll<HTMLElement>(".app");
+const $apps = document.querySelectorAll<HTMLElement>('.app');
 $apps.forEach(($app) => {
   const root = createRoot($app);
-  const config = $app.dataset.config
-    ? JSON.parse($app.dataset.config)
-    : undefined;
 
   root.render(
     <StrictMode>
-      <App config={config} />
-    </StrictMode>,
+      <App />
+    </StrictMode>
   );
 });
