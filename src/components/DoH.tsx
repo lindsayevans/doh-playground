40c5c31360f0f379632750c0798afded8b92b304
Lindsay Evans <lindsay.evans@boomworks.com.au>
2025-10-30 11:01:31 +1100

    feat: more bits

---
+++ src/components/DoH.tsx
@@ -0,0 +1,210 @@
+import React, { useEffect, useState } from 'react';
+
+import {
+  Alert,
+  Button,
+  Container,
+  createTheme,
+  Flex,
+  Loader,
+  MultiSelect,
+  NativeSelect,
+  TextInput,
+} from '@mantine/core';
+
+import '@mantine/core/styles.css';
+import 'mantine-datatable/styles.css';
+
+const theme = createTheme({
+  fontFamily: 'Inter, sans-serif',
+  primaryColor: 'cyan',
+});
+
+import { DnsRecord, DnsRecordTypes } from '../DnsRecord';
+import { DataTable } from 'mantine-datatable';
+import { useSearchParams } from 'react-router-dom';
+import { IconAlertCircle, IconInfoCircle } from '@tabler/icons-react';
+
+const resolvers = [
+  {
+    name: 'Cloudflare',
+    url: 'https://cloudflare-dns.com/dns-query',
+  },
+  {
+    name: 'Google',
+    url: 'https://dns.google/resolve',
+  },
+];
+
+export const DoH: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const searchDomain = searchParams.get('domain');
+  const searchTypes = searchParams.getAll('types');
+
+  const [domain, setDomain] = useState(searchDomain || 'example.com');
+  const [resolver, setResolver] = useState(
+    searchParams.get('resolver') || resolvers[0].name
+  );
+  const [types, setTypes] = useState<string[]>(
+    searchTypes && searchTypes.length > 0 ? searchTypes : ['A', 'AAAA', 'CNAME']
+  );
+
+  const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState<DnsRecord[]>([]);
+  const [error, setError] = useState<string>();
+  const [info, setInfo] = useState<string>();
+
+  const allTypes = Object.values(DnsRecordTypes)
+    .filter((x) => isNaN(x as number))
+    .map((x) => x.toString());
+
+  const buildQuery = async (
+    name: string,
+    type: string,
+    resolverUrl?: string
+  ) => {
+    const response = await fetch(`${resolverUrl}?name=${name}&type=${type}`, {
+      headers: { accept: 'application/dns-json' },
+    });
+
+    if (response.status !== 200) {
+      throw new Error(`Error response from resolver: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  };
+
+  const getResolverUrl = (name: string) => {
+    return resolvers.find((x) => x.name === name)?.url || resolvers[0].url;
+  };
+
+  const onQueryClick = () => {
+    setSearchParams({
+      resolver,
+      domain,
+      types,
+    });
+
+    runAllQueries();
+  };
+
+  const runAllQueries = async () => {
+    setLoading(true);
+    setResults([]);
+    setInfo(undefined);
+    setError(undefined);
+
+    try {
+      const queries = types.map((x) =>
+        buildQuery(domain, x, getResolverUrl(resolver))
+      );
+      const queryResults = await Promise.all(queries);
+
+      const results: DnsRecord[] = [];
+      queryResults.forEach((result) => {
+        if (
+          result.Status === 0 &&
+          result.Answer !== undefined &&
+          result.Answer.map
+        ) {
+          results.push(
+            ...result.Answer.map((answer: Record<string, string>) => ({
+              id: btoa(answer.type + answer.name + answer.data),
+              type: DnsRecordTypes[answer.type as unknown as number],
+              name: answer.name,
+              data: answer.data,
+              ttl: answer.TTL,
+            }))
+          );
+        }
+      });
+
+      if (results.length === 0) {
+        setInfo('No results returned from resolver');
+      }
+
+      setResults(results);
+    } catch (e: any) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    runAllQueries();
+  }, [searchDomain]);
+
+  return (
+    <Container size="lg">
+      <Flex
+        direction={{ base: 'column', sm: 'row' }}
+        align={{ base: 'stretch', sm: 'flex-end' }}
+        gap="sm"
+        mb="md"
+        mt="xl"
+      >
+        <NativeSelect
+          label="Resolver"
+          value={resolver}
+          data={resolvers.map((x) => x.name)}
+          onChange={(e) => setResolver(e.target.value)}
+        />
+        <TextInput
+          label="Domain"
+          placeholder="e.g. example.com"
+          value={domain}
+          onChange={(event) => setDomain(event.currentTarget.value)}
+        />
+        <MultiSelect
+          label="Types"
+          data={allTypes}
+          value={types}
+          onChange={setTypes}
+          clearable
+          searchable
+        />
+        <Button variant="filled" onClick={() => onQueryClick()}>
+          Query
+        </Button>
+      </Flex>
+
+      {results && results.length > 0 && (
+        <>
+          <DataTable
+            columns={[
+              { accessor: 'type' },
+              { accessor: 'name' },
+              { accessor: 'data' },
+              { accessor: 'ttl', title: 'TTL' },
+            ]}
+            records={results}
+          ></DataTable>
+          {/* <pre>{JSON.stringify(results, null, 2)}</pre> */}
+        </>
+      )}
+
+      {loading && <Loader color="teal" />}
+
+      {info && (
+        <Alert variant="light" color="indigo" icon={<IconInfoCircle />}>
+          {info}
+        </Alert>
+      )}
+
+      {error && (
+        <Alert
+          variant="light"
+          color="pink"
+          title="Error querying resolver"
+          icon={<IconAlertCircle />}
+        >
+          {error}
+        </Alert>
+      )}
+    </Container>
+  );
+};
